4927a0379347876558f2fb0b6ac516ce9943242e
Manuel Lemos <mlemos@acm.org>
2022-05-21 15:27:18 -0300

    Changed the test failure messages to tell which PHP extensions may be missing.

--- drivers/ttf.php
+++ drivers/ttf.php
@@ -10,10 +10,15 @@ class feature_test_ttf_class extends feature_test_driver_class
 {
 	Function Process()
 	{
+		if(!function_exists($function = 'ImageCreate'))
+		{
+			$this->results = 'DISABLED: The GD extension is not available in this PHP installation as expected because the '.$function.' is missing.';
+		}
+		return true;
 		if(!function_exists($function = 'ImageTTFBBox')
 		|| !function_exists($function = 'ImageTTFText'))
 		{
-			$this->results = 'DISABLED: The function '.$function.' is not available in this PHP installation as expected.';
+			$this->results = 'DISABLED: The FreeType library is not used to build this PHP version because the function '.$function.' is not available as expected.';
 		}
 		return true;
 	}
