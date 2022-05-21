c551d243c75707472b736065e90596da5d59fa30
Manuel Lemos <mlemos@acm.org>
2022-05-21 15:29:38 -0300

    Changed the tests to check if the host of the test URL is valid and if the ssl_url option is set. Made the ssl_url_user a configurable option.

--- drivers/ssl_client.php
+++ drivers/ssl_client.php
@@ -20,18 +20,46 @@ class feature_test_ssl_client_class extends feature_test_driver_class
 		}
 		else
 		{
-			$url = str_replace('{account}', 'mlemos', $this->options->ssl_url);
-			$context = array(
-				'ssl' => array(
-					'verify_peer' => false,
-					'verify_peer_name' => false
-				)
-			);  
-			$success = file_get_contents($url, false, stream_context_create($context));
-			if(!$success)
+			if(!IsSet($this->options->ssl_url_user))
 			{
-				$error = error_get_last();
-				$this->results = 'Accessing OpenID URL failed ('.$url.') with error: '.$error['message'];
+				$this->results = 'the ssl_url_user option is not set';
+			}
+			elseif(!IsSet($this->options->ssl_url))
+			{
+				$this->results = 'the ssl_url option is not set.';
+			}
+			else
+			{
+				$url = str_replace('{account}', $this->options->ssl_url_user, $this->options->ssl_url);
+				$parsed = parse_url($url);
+				if($parsed === FALSE)
+				{
+					$this->results = 'the URL to test SSL '.$url.' is not valid.';
+				}
+				else
+				{
+					$host = $parsed['host'];
+					$ip = gethostbyname($host);
+					if($ip === $host)
+					{
+						$this->results = 'It is not possible to resolve the host name '.$host.' the of the URL to test SSL '.$url.' .';
+					}
+					else
+					{
+						$context = array(
+							'ssl' => array(
+								'verify_peer' => false,
+								'verify_peer_name' => false
+							)
+						);  
+						$success = file_get_contents($url, false, stream_context_create($context));
+						if(!$success)
+						{
+							$error = error_get_last();
+							$this->results = 'Accessing OpenID URL failed ('.$url.') with error: '.$error['message'];
+						}
+					}
+				}
 			}
 		}
 		return true;
